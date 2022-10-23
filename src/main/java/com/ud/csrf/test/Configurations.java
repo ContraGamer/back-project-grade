51f40ec1d36bc249546d8b1b4ba8ce654bfaa52d
Alex Gonzalez <alexgonzalez@MacBook-Air-de-Alex.local>
2022-10-23 16:36:05 -0500

    Settings to configurations

---
+++ src/main/java/com/ud/csrf/test/Configurations.java
@@ -0,0 +1,21 @@
+package com.ud.csrf.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Configurations {
+    @Bean
+    public WebMvcConfigurer CORSConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT","DELETE");
+            }
+        };
+    }
+}
